23c6b674483975afd0832729e7554c6126c509d6
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-02-16 19:37:12 +0530

    alt tag change in image component

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -101,7 +101,7 @@ const Testimonials = () => {
 										<div className="mb-4">
 											<div className="flex items-center gap-4">
 												<Avatar>
-													<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+													<AvatarImage src="https://github.com/shadcn.png" alt="profile" />
 													<AvatarFallback>CN</AvatarFallback>
 												</Avatar>
 												<div>
