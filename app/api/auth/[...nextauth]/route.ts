207d431da19956b8bd37b0d5ab3bb34ecf0aa58c
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-02-25 03:42:37 +0530

    build export error fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-
-export const authOptions: NextAuthOptions = {
-	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID as string,
-			clientSecret: process.env.GITHUB_SECRET_ID as string,
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_SECRET_ID as string,
-		}),
-	],
-	secret: process.env.NEXTAUTH_SECRET as string,
-}
+import NextAuth from 'next-auth'
+import { authOptions } from '@/auth'
 
 const handler = NextAuth(authOptions)
 
---
+++ auth.ts
@@ -0,0 +1,17 @@
+import { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_SECRET_ID as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_SECRET_ID as string,
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET as string,
+}
