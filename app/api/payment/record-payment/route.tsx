f90aab57a85df4596aa514f81a780db23e75271d
nikhil-clickzy <nikhil.clickzy@gmail.com>
2024-06-06 21:52:57 +0530

    razorpay test build

--- app/api/payment/record-payment/route.tsx
+++ app/api/payment/record-payment/route.tsx
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
 
-import prisma from '@/utils/database/dbConnect'
-
 export async function GET(req: Request) {
 	const signature = req.headers.get('x-razorpay-signature')
 	console.log('a reques tis made th')
