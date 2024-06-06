2d009e622ab913a812d7274c28bbdb47dd0b95d2
nikhil-clickzy <nikhil.clickzy@gmail.com>
2024-06-07 03:56:22 +0530

    record-payment route changes

--- app/api/payment/record-payment/route.tsx
+++ app/api/payment/record-payment/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
 	const signature = req.headers.get('x-razorpay-signature')
 	console.log('this is from the payment success route')
 	console.log('signature', signature)
