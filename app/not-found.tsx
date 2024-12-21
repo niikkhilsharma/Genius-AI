1ffcd16b6c43b1083b5adae48987ec370c9f2fec
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2024-12-21 17:59:10 +0530

    fixed the sidebar missing prop build error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,29 @@
-import Navbar from '@/components/navbar'
-import Sidebar from '@/components/sidebar'
-import { Toaster } from 'react-hot-toast'
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+import { Toaster } from "react-hot-toast";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth";
+import prisma from "@/lib/database/dbConnect";
 
-export default function NotFound() {
-	return (
-		<>
-			<div className="h-full">
-				<Toaster position="top-center" reverseOrder={false} />
-				<div className="hidden md:block md:fixed md:w-72 md:inset-y-0 bg-gray-900">
-					<Sidebar />
-				</div>
-				<main className="md:pl-72">
-					<Navbar />
-					<div className="w-full flex justify-center items-center font-bold">This page is not available</div>
-				</main>
-			</div>
-		</>
-	)
+export default async function NotFound() {
+  const session = await getServerSession(authOptions);
+  const userProfile = await prisma.profile.findUnique({
+    where: { id: session?.user.profile.id },
+  });
+  return (
+    <>
+      <div className="h-full">
+        <Toaster position="top-center" reverseOrder={false} />
+        <div className="hidden bg-gray-900 md:fixed md:inset-y-0 md:block md:w-72">
+          <Sidebar profile={userProfile} />
+        </div>
+        <main className="md:pl-72">
+          <Navbar />
+          <div className="flex w-full items-center justify-center font-bold">
+            This page is not available
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
